0618381a065f84d8a34bd74a21c41b953bb75a97
Gustavs T <g@compose.lv>
2020-04-25 22:56:14 +0100

    Use Spec in fiddle defs

--- src/hyperfiddle/def.clj
+++ src/hyperfiddle/def.clj
@@ -2,12 +2,24 @@
   (:require
     [taoensso.timbre :as timbre]
     [clojure.spec.alpha :as s]
-    [hyperfiddle.etc.expr :refer :all]
-    [contrib.etc :refer [with-ns trim-str]]
-    [hyperfiddle.etc.etc :refer :all]
-    ;[hypercrud.browser.base :as H]
-    ))
+    [contrib.expr :refer :all]
+    [contrib.data :refer [qualify trim-str]]))
 
+
+(declare def!) ; main definitions
+(declare attr)
+(declare fiddle)
+(declare project)
+
+(declare read-def) ; parsing
+(declare read-schema)
+(declare read-props)
+(declare read-links)
+(declare map-attr)
+(declare def!)
+(declare update-context!)
+(declare annotate-source)
+(declare get-source-for)
 
 (defonce *defs (atom {}))
 (defonce *file-ref (atom {}))
@@ -15,23 +27,6 @@
 (defn get-schema [id] (get-in @*defs [:schema id]))
 (defn get-fiddle [id] (get-in @*defs [:fiddle id]))
 (defn get-a [type id] (get-in @*defs [type id]))
-
-(declare ; main definitions
-  def!
-  attr
-  fiddle
-  project)
-
-(declare ; parsing
-  read-def
-  read-schema
-  read-props
-  read-link
-  map-attrs
-  def!
-  update-context!
-  annotate-source
-  get-var-source)
 
 
 (defn def! [type ident form val]
@@ -42,9 +37,7 @@
   (update-context!)
   (swap! *defs
          assoc-in [type ident]
-         val
-    ;(vary-meta val assoc :source (get-var-source ident (@*defs @*file-ref)))
-    )
+         (vary-meta val assoc :source (get-source-for form (@*defs @*file-ref))))
   nil
   )
 
@@ -58,7 +51,7 @@
 
 (defmacro fiddle [ident & attrs]
   (def! :fiddle ident &form
-    (read-def :fiddle ident attrs)))
+        (read-def :fiddle ident attrs)))
 
 (defmacro project [ident & attrs]
   (def! :project ident &form
@@ -73,131 +66,180 @@
     :& (s/* any?)))
 
 (s/def ::def
-  (map-spec
-    reduce-result
-    (s/cat
-      :doc (s/? string?)
-      :args (s/? (s/coll-of any?))
-      :& (s/* any?))))
-
-(s/def ::expr
-  (some-fn keyword? seq?))
-
-(s/def ::links
-  (map-spec reduce-result (s/map-of any? (s/or :_ ::link-expr :* (s/coll-of ::link-expr)))))
-
-(s/def ::link-expr
   (s/cat
-    :class (s/? (some-fn keyword? set?))
-    :fiddle (s/? ::expr)
+    :doc (s/? string?)
+    :args (s/? (s/coll-of any?))
     :& (s/* any?)))
 
+(s/def ::expr
+  (s/and seq?
+    (comp symbol? first)))
+
+(s/def ::link-expr
+  (read-alt
+    (s/cat :class #{:hf/new :hf/iframe :hf/edit}
+      :fiddle ::expr
+      :& (s/* any?))
+    (s/cat :class #{:hf/remove}
+      :& (s/* any?))
+    (s/cat :class (s/? set?)
+      :fiddle (read-vec ::expr)
+      :& (s/* any?))))
+
+(s/def ::links
+  (s/map-of
+    any?
+    (read-alt (s/& ::link-expr (s/conformer vector))
+      (s/+ (s/spec ::link-expr)))))
 
 (defn update-context! []
   (when (not= @*file-ref *file*)
+    (timbre/info "loading fiddle defs" *file*)
     (reset! *file-ref *file*)
-    (swap! *defs assoc *file* (annotate-source (slurp *file*))))
+    (swap! *defs assoc *file* (annotate-source (slurp (or (clojure.java.io/resource *file*) *file*)))))
   nil)
 
 (defn annotate-source [input & [origin]]
   (let [r (clojure.lang.LineNumberingPushbackReader. (java.io.StringReader. input))]
     (when (:line origin) (.setLineNumber r (:line origin)))
     (take-while #(not= :. %)
-      (repeatedly (fn [] (let [[val val-str] (read+string r false :.)]
+      (repeatedly (fn [] (let [[val val-str] (read+string {:eof :. :read-cond :allow} r)]
                            (cond-> val (instance? clojure.lang.IObj val) (vary-meta assoc :source val-str))))))))
 
-(defn get-var-source [ident source]
-  (timbre/debug 'get-var-source ident source)
-  (assert (-> ident meta :line))
-  (some->
-    (drop-while
-      (fn [el] (< (-> el meta :line)
-                  (-> ident meta :line)))
-      source)
-    first))
+(defn get-source-for [form source]
+  (if (and source (-> form meta :line))
+    (some->
+      (drop-while
+        (fn [el] (and (-> el meta :line)
+                      (< (-> el meta :line)
+                         (-> form meta :line))))
+        source)
+      first)))
 
 (defn read-schema [attrs]
-  (into {}
-    (for [[attr desc] attrs]
-      (do (assert (qualified-keyword? attr))
-          (let [desc (read-spec ::schema desc)]
-            [attr
-             (into {:db/ident attr
-                    :cardinality :db.cardinality/one}
-               (concat
-                 (when (:type desc)
-                   (let [[_ type mod] (re-matches #"(.+?)(\*?)" (name (:type desc)))]
-                     (merge {:valueType (with-ns 'db.type type)}
-                       (when (= mod "*") {:cardinality :db.cardinality/many}))))
-                 (when (:doc desc) {:doc (trim-str (:doc desc))})
-                 (when (-> desc :mod #{:many}) {:cardinality :db.cardinality/many})
-                 (when (-> desc :mod #{:unique}) {:unique :db.unique/identity})
-                 (when-let [[& {:as rest}] (:& desc)] rest)))]
-            )))))
+  (reduce-kv
+    (fn [schema attr desc]
+      (assert (qualified-keyword? attr))
+      (assoc
+        schema attr
+        (let [desc (read-spec ::schema desc)]
+          (merge
+            {:db/ident    attr
+             :cardinality :db.cardinality/one}
+            (when (:type desc)
+              (let [[_ type mod] (re-matches #"(.+?)(\*?)" (name (:type desc)))]
+                (merge {:valueType (qualify 'db.type type)}
+                  (when (= mod "*") {:cardinality :db.cardinality/many}))))
+            (when (-> desc :mod #{:many}) {:cardinality :db.cardinality/many})
+            (when (-> desc :mod #{:unique}) {:unique :db.unique/identity})
+            (select-keys desc [:doc])
+            (when-let [[& {:as rest}] (:& desc)] rest)))))
+    {}
+    attrs))
 
-(defn read-props [body]
+(defn read-attrs [body]
   (->> (read-spec (s/* (s/cat :k keyword? :v (s/+ (comp not keyword?)))) body)
        (map (fn [{:keys [k v]}] [k v]))
        (into {})))
 
 (defn read-def [type ident attrs]
   (s/assert qualified-keyword? ident)
-  ;(timbre/debug :parsing attrs)
   (let [attrs (read-spec ::def attrs)]
-    (map-attrs type
-      (concat {:ident ident}
-        (when (= type :fiddle) {:fiddle/type :blank})
-        (when (:doc attrs) {:doc (:doc attrs)})
-        (when (:args attrs) {:args (:args attrs)})
-        (dissoc attrs :id :doc :&)
-        (read-props (:& attrs))))))
+    (reduce
+      (fn [x [k v]]
+        ;(timbre/debug :! ident x (map-attr type k v))
+        (merge x (map-attr type k v))) {}
+      (merge
+        {:ident ident}
+        (case type :fiddle {:fiddle/source (symbol (.name *ns*))}
+                   nil)
+        (dissoc attrs :&)
+        (let [attrs (read-attrs (:& attrs))]
+          (merge
+            attrs
+            (when (:links attrs)
+              {:links (read-links (one (:links attrs)))})))))))
 
-(defn one [v]
-  {:pre [(= 1 (count v))]}
-  (first v))
+(defn read-links [links]
+  (let [links (read-spec ::links links)]
+    (-> (seq links)
+        (over (fn [[k v]] (over v #(do [k %]))))
+        (#(apply concat %))
+        (over (fn [[path link]]
+                (merge {:path path}
+                       (dissoc link :&)
+                       (when-let [[& {:as rest}] (:& link)] rest)))))))
 
-(defn normalize-str [v]
+(defn map-str [v]
   (trim-str v))
 
-(defn normalize-code [k v]
+(defn map-code [k v]
   (cond (or (vector? v) (list? v)) (clojure.string/join "\n" (map str v))
-        (string? v) (normalize-str v)))
+        (string? v) (map-str v)))
 
-(defn normalize-expr [v]
-  (cond (and (seq? v) (= (first v) `quote)) (second v)
-        () v))
+(defn map-expr [v]
+  (cond (form? v)
+        (cond (= (first v) `quote) (second v)
+              (and (= (first v) `unquote)
+                   (qualified-keyword? (second v))) (second v))
+        ()
+        v))
+
+(defn link-expr [v]
+  {:fiddle/ident (map-expr v)})
+
+(defn requote-expr [x]
+  ; ...
+  )
 
 (defn map-attr [type k v]
+  (s/assert #{:project :fiddle :attr :link} type)
+  (s/assert keyword? k)
+  (s/assert (comp not nil?) v)
+
   (as->
     (case [type k]
       [:project :ident] :db/ident
       [:attr :ident] :attribute/ident
       [:fiddle :ident] :fiddle/ident
-      [:fiddle :code] :fiddle/cljs-ns
-      [:fiddle :pull] (let [[db q] (->> v normalize-expr (split-with (some-fn string? seq?)))]
+
+      [:fiddle :pull] (let [[db q] (->> v map-expr (split-with (some-fn string? seq?)))]
                         (cond->
                           {:fiddle/type :entity
                            :fiddle/pull (one q)}
                           (not (empty? db)) (merge {:fiddle/pull-database (one db)})))
       [:fiddle :query] {:fiddle/type  :query
-                        :fiddle/query (-> v normalize-expr one)}
-      [:fiddle :links] {:fiddle/links (->> v one (read-spec ::links))}
+                        :fiddle/query (-> v one map-expr)}
+      [:fiddle :code] :fiddle/cljs-ns
+      [:fiddle :links] {:fiddle/links
+                        (mapv (fn [link]
+                                (reduce-kv (fn [x k v] (merge x (map-attr :link k v))) {} link))
+                          v)}
+      [:fiddle :with] {:fiddle/with (one v)}
+      [:link :fiddle] {:link/fiddle
+                       (link-expr v)}
+      [:link :class] {:link/class
+                       (let [x (-> v map-expr)]
+                         (cond (vector? x) x
+                               (keyword? x) [x]
+                               (set? x) (vec x)))}
+
       (cond
         (qualified-keyword? k) k
         (= type :schema) k
-        () (with-ns type k)))
-    remapped
-    (if (keyword? remapped)
-      {remapped v}
-      remapped)
-    (->> remapped
-        (map (let [k' k] (fn [[k v]]
-                           [k (cond (k' #{:code :cljs-ns :renderer}) (normalize-code k' v)
-                                    (string? v) (normalize-str v)
-                                    () v)])))
-        (into {}))))
+        () (qualify type k)))
 
-(defn map-attrs [type attrs]
-  (apply merge
-    (map (fn [[k v]] (map-attr type k v))
-         attrs)))
+    remap
+
+    (->> (if (keyword? remap)
+           {remap v}
+           remap)
+         (reduce-kv
+           (let [k' k]
+             (fn [x k v]
+               (assoc x k
+                      (cond (k' #{:code :cljs-ns :renderer :markdown :css}) (map-code k' v)
+                            (k #{:query :pull :formula}) (map-code k v)
+                            (string? v) (map-str v)
+                            () v))))
+           {}))))
