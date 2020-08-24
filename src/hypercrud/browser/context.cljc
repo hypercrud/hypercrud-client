hypercrud/hypercrud-client

commit a4347ca5e8fe2a1f5fca43189050dc478c216d21

route-defaults-symbolic made available to hf/tx on client #1104

src/hypercrud/browser/context.cljc
@@ -696,20 +696,20 @@ a speculative db/id."
 
 (defn derive-for-args-rendering
   "Build a new context with route defaults as result so they can be rendered as a form."
-  [{:keys [:hypercrud.browser/route :hypercrud.browser/route-defaults] :as ctx}]
+  [{:keys [:hypercrud.browser/route :hypercrud.browser/route-defaults-hydrated] :as ctx}]
   (let [args-spec (some-> ctx :hypercrud.browser/fiddle deref :fiddle/ident s/get-spec :args)]
     (-> ctx
-        (dissoc :hypercrud.browser/route-defaults) ; avoid potential recursion
+        (dissoc :hypercrud.browser/route-defaults-hydrated) ; avoid potential recursion
         (assoc
-         ;; We want to render as a form, so qfind is FindScalar
-         :hypercrud.browser/qfind (r/pure (fiddle/shape 'FindScalar))
-         ;; But we want to validate against s/cat, so we set the default route as sexp […]
-         :hypercrud.browser/result (r/fmap rest route)) ; drop ƒ
+          ;; We want to render as a form, so qfind is FindScalar
+          :hypercrud.browser/qfind (r/pure (fiddle/shape 'FindScalar))
+          ;; But we want to validate against s/cat, so we set the default route as sexp […]
+          :hypercrud.browser/result (r/fmap rest route-defaults-hydrated)) ; drop ƒ
         (as-> ctx
             ;; validate against default route
           (assoc ctx :hypercrud.browser/validation-hints (r/track (partial validation-hints-enclosure! args-spec) ctx))
           ;; set result back as a map of {field name => field value}
-          (assoc ctx :hypercrud.browser/result (r/fmap to-map route-defaults))
+          (assoc ctx :hypercrud.browser/result (r/fmap to-map route-defaults-hydrated))
           (assoc ctx :hypercrud.browser/result-enclosure (r/track result-enclosure! ctx)))
         (index-result))))
 

src/hypercrud/browser/base.cljc
@@ -38,7 +38,8 @@
   "Extract a fiddle response into [route-defaults result]"
   [r-response]
   [(from-result (r/fmap first r-response)) ; completed route
-   (from-result (r/fmap second r-response))])
+   (from-result (r/fmap second r-response))
+   (from-result (r/fmap last r-response))])
 
 
 ; internal bs abstraction to support hydrate-result-as-fiddle
@@ -58,10 +59,10 @@
           ;_ (timbre/debug "request" @r-request)
           r-response (from-result @(r/apply-inner-r (r/fmap->> r-request (nil-or-hydrate+ rt pid))))
           ;_ (timbre/debug "result" @r-fiddle :-> @r-result)
-          [r-route-defaults r-result] (explode-result r-response)]
+          [r-route-defaults r-route-defaults-hydrated r-result] (explode-result r-response)]
 
       ;; FIXME this is a hack, a new runtime impl is needed
-      #?(:clj (state/dispatch! rt [:partition-route-defaults pid @r-route-defaults]))
+      #?(:clj (state/dispatch! rt [:partition-route-defaults pid @r-route-defaults-hydrated]))
 
       ; fiddle request can be nil for no-arg pulls (just draw readonly form)
       (context/result
@@ -71,7 +72,8 @@
               ; because it is not, this is obviously fragile and will break on any change to the route
               ; this is acceptable today (Jun-2019) because changing a route in ANY way assumes the entire iframe will be re-rendered
               (assoc :hypercrud.browser/route (r/pure (vec route))) ; vec for direct positional access
-              (assoc :hypercrud.browser/route-defaults r-route-defaults)
+              (assoc :hypercrud.browser/route-defaults-hydrated r-route-defaults-hydrated)
+              (assoc :hypercrud.browser/route-defaults-symbolic r-route-defaults)
               (context/fiddle+ r-fiddle)))
         r-result))))
 

src/hyperfiddle/api.cljc
@@ -200,7 +200,7 @@
   "Get route, with default values. Rember default route values get overwritten by
   user-provided values"
   [ctx]
-  @(:hypercrud.browser/route-defaults ctx))
+  @(:hypercrud.browser/route-defaults-hydrated ctx))
 
 ;; multiple params can depend on each other so set defaults centrally per fiddle
 (defmulti defaults first)

src/hyperfiddle/io/datomic/hydrate_requests.clj
@@ -89,7 +89,7 @@
             hf/*$*      (get-db get-secure-db-with pid "$")
             hf/*get-db* (partial get-db get-secure-db-with pid)]
     (let [defaults (hf/defaults route)]
-      [(hf/view-defaults defaults) (eval-as-fexpr! defaults)])))
+      [defaults (hf/view-defaults defaults) (eval-as-fexpr! defaults)])))
 
 ; todo i18n
 (def ERROR-BRANCH-PAST ":hyperfiddle.error/basis-stale Branching the past is currently unsupported, please refresh your basis by refreshing the page")

src/hyperfiddle/ui.cljs
@@ -566,7 +566,7 @@ nil. call site must wrap with a Reagent component"          ; is this just hyper
   [val ctx & [props]]
   {:pre [(not= val clojure.core/val)]}                      ; check for busted call while we migrate away from this param
   [:<>
-   (when (and (some? (:hypercrud.browser/route-defaults ctx))
+   (when (and (some? (:hypercrud.browser/route-defaults-hydrated ctx))
               (some? (spec/args ctx)))
      [render-args ctx props])
    (doall
@@ -615,7 +615,7 @@ nil. call site must wrap with a Reagent component"          ; is this just hyper
 (letfn [(render-edn [data]
           [contrib.ui/async-code {:value data :read-only true}])]
   (defn ^:export fiddle-api [_ {rt             :runtime
-                                route-defaults :hypercrud.browser/route-defaults
+                                route-defaults :hypercrud.browser/route-defaults-hydrated
                                 :as            ctx} & [props]]
     [:div.hyperfiddle.display-mode-api (select-keys props [:class])
      (render-edn (some-> (:hypercrud.browser/result ctx) deref))

commit 9776d8d7c0ecdc8b77fa0d02bcb2262308073a67

Fix regression caused by a4347ca5
Hydrated route defaults were validated instead of the actual user input (route).

src/hypercrud/browser/context.cljc
@@ -704,7 +704,7 @@ a speculative db/id."
           ;; We want to render as a form, so qfind is FindScalar
           :hypercrud.browser/qfind (r/pure (fiddle/shape 'FindScalar))
           ;; But we want to validate against s/cat, so we set the default route as sexp […]
-          :hypercrud.browser/result (r/fmap rest route-defaults-hydrated)) ; drop ƒ
+          :hypercrud.browser/result (r/fmap rest route)) ; drop ƒ
         (as-> ctx
             ;; validate against default route
           (assoc ctx :hypercrud.browser/validation-hints (r/track (partial validation-hints-enclosure! args-spec) ctx))